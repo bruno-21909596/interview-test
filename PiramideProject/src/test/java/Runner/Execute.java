a81f36c0c43759148815f96a7855da00145a8874
Bruninho <blapeoficial@gmail.com>
2021-12-12 14:51:16 -0300

    updated information

--- PiramideProject/src/test/java/Runner/Execute.java
+++ PiramideProject/src/test/java/Runner/Execute.java
@@ -1,17 +1,22 @@
 package Runner;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import org.junit.runners.Suite;
+import Steps.Post;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	   Post.class})
+
+/*Other type of RunWith that is commonly used by me when using cucumber is: 
+ * @CucumberOptions(
 features = "src/test/resources",
+tags = "@tags used"
 glue = "Steps",
-plugin = {"pretty","html:target/report.html","json:target/report.json"},
+@plugin = {"pretty","html:target/report.html","json:target/report.json"},
 dryRun = false,
 monochrome = true
-)
+)*/
 
 public class Execute {
 
